4fb376d08617b278b455121de79d67f1e60e7324
norzyNorth <musnorzy@gmail.com>
2024-02-16 12:45:08 +0300

    rot

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import { Quaternion } from "three";
 class Player {
   uuid: string;
   x: number = 0;
   y: number = 0;
   z: number = 0;
-
+  rot: Quaternion = new Quaternion();
   constructor(socketInfo: Socket) {
     this.uuid = socketInfo.id;
   }
@@ -15,19 +16,26 @@ class Player {
     this.x = updateData.x;
     this.y = updateData.y;
     this.z = updateData.z;
+    this.rot = new Quaternion(
+      updateData.rot.x,
+      updateData.rot.y,
+      updateData.rot.z,
+      updateData.rot.w
+    );
   }
 }
 interface UpdateData {
   x: number;
   y: number;
   z: number;
+  rot: Quaternion;
 }
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 const players: Player[] = [];
 
@@ -38,11 +46,10 @@ io.on("connection", (socket) => {
   console.log(`Player Connected: ${socket.id}`);
 
   socket.on("updateData", (updateData: UpdateData) => {
-    // console.log(updateData)
     const index = players.findIndex((player) => player.uuid === socket.id);
     if (index !== -1) {
       players[index].update(updateData);
-      socket.broadcast.emit('playerUpdated', players[index])
+      socket.broadcast.emit("playerUpdated", players[index]);
     }
     console.log(players)
   });
@@ -52,8 +59,8 @@ io.on("connection", (socket) => {
       io.emit("playerDisconnected", players[index]);
       players.splice(index, 1);
       console.log(`Player Disconnected: ${socket.id}`);
-     }
+    }
   });
 });
 
-io.listen(3000)
+io.listen(3000);
